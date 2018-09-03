930731b9feffb23a1c69405bc94408f7fccfc322
Bernard Kim <kimb2674@gmail.com>
2018-09-03 16:17:43 -0700

    ...

--- server/src/controllers/portfolio/portfolio.go
+++ server/src/controllers/portfolio/portfolio.go
@@ -15,14 +15,15 @@ import (
 
 var db *xorm.Engine
 
-type PortfolioData struct {
-	Portfolio Portfolios.Portfolio `json:"portfolio"`
-}
+// type PortfolioData struct {
+// 	Portfolio Portfolios.Portfolio `json:"portfolio"`
+// }
 
 func Init(DB *xorm.Engine) {
 	db = DB
 }
 
+// GetStocks returns an array of transactions made by a user
 func GetStocks(w http.ResponseWriter, r *http.Request) {
 	tokenVal := r.Header.Get("X-App-Token")
 	fmt.Println(r.Header.Get("Cookie"))
@@ -34,6 +35,7 @@ func GetStocks(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// get user from token
 	user, err := jwt.GetUserFromToken(db, tokenVal)
 	if err != nil {
 		log.Println(err)
@@ -41,15 +43,18 @@ func GetStocks(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	portfolio := Portfolios.Portfolio{}
-	err = ORM.Find(db, &Portfolios.Transaction{UserId: user.ID}, &portfolio)
+	// get all transactions made by user
+	var transactionList []Portfolios.Transaction
+
+	err = ORM.Find(db, &Portfolios.Transaction{UserId: user.ID}, &transactionList)
 	if err != nil {
 		log.Println(err)
 		http.Error(w, "unable to get portfolio from database", http.StatusUnauthorized) //TODO: status code
 		return
 	}
 
-	packet, err := json.Marshal(portfolio)
+	// packet, err := json.Marshal(portfolio)
+	packet, err := json.Marshal(transactionList)
 	if err != nil {
 		log.Println(err)
 		http.Error(w, "Unable to marshal json.", http.StatusUnauthorized) // TODO: status code
